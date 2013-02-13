260cf94e90ee6613e2273e618a91c475ccbe208f
Anuj Seth <greenthunderleaf@gmail.com>
2013-02-13 22:42:28 +0530

    Added the tab bar's interface.

--- PARTabBar.h
+++ PARTabBar.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
+@class PARTabBar;
+
+@protocol PARTabBarDelegate <NSObject>
+
+// required cause otherwise its kind of useless
+@required
+
+// The user changed to a tab with index. Only
+// called if the new selection is different from
+// the current one.
+- (void)tabBar:(PARTabBar *)tabBar didSelectItemIndex:(NSUInteger)itemIndex;
+
+@end
+
+// A custom tab bar for ready made tabs. Useful
+// when you want to directly use your exported
+// tab images from Inkscape, Photoshop etc.
 @interface PARTabBar : UIView
+
+@property (nonatomic, assign) id<PARTabBarDelegate> delegate;
+// the count should be the same, and the image
+// frames should add up to the tab bar frame,
+// else it explodes
+@property (nonatomic, strong) NSArray *normalImages;
+@property (nonatomic, strong) NSArray *activeImages;
+@property (nonatomic, assign) NSUInteger initiallySelectedIndex;
 
 @end
