d4cf8ca88bb88f96cfe8a6116653158daa7956ec
Anuj Seth <greenthunderleaf@gmail.com>
2013-02-13 22:36:50 +0530

    Added a new class for the custom tab bar. Going to adapt the iPhone Hiit Stopwatch code and make it generic enough to be used anywhere. We need it for the iPad version for example.

---
+++ PARTabBar.h
@@ -0,0 +1,13 @@
+//
+//  PARTabBar.h
+//  PARTabBar
+//
+//  Created by Anuj Seth on 2/13/13.
+//  Copyright (c) 2013 Parabolic River. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PARTabBar : UIView
+
+@end
---
+++ PARTabBar.m
@@ -0,0 +1,31 @@
+//
+//  PARTabBar.m
+//  PARTabBar
+//
+//  Created by Anuj Seth on 2/13/13.
+//  Copyright (c) 2013 Parabolic River. All rights reserved.
+//
+
+#import "PARTabBar.h"
+
+@implementation PARTabBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
